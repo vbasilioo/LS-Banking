cab111248b7886454716489906c2bd355283c883
Vinícius Basílio <vbasilio2019@gmail.com>
2022-12-17 19:27:57 -0300

    create new navbar responsive

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -4,6 +4,15 @@
     <li><a href="/user/create">Cadastro</a></li>
 </ul>
 
-<div id="#status_login">
-    Bem-vindo, visitante.
-</div>
+<nav>
+    <div class="nav-bar">
+        <span class="logo"><a href="#">Logo</a></span>
+        <div class="menu">
+            <ul class="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/user/create">Register</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
